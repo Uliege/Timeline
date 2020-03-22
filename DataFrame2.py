50a13cfbbd7eb5feeb7e0b23c2181a7f7371a2a0
malcivar <44213991+malcivar@users.noreply.github.com>
2020-03-21 22:56:16 -0500

    correccion errores
    correccion errores al momento de leer ciertos archivos

--- DataFrame2.py
+++ DataFrame2.py
@@ -10,13 +10,24 @@ import pyreadr
 
 directorio_entrada="C:/Users/Andres/Desktop/Tesis/Marcos/Pruebas/"
 
-for k in range(0,len(glob.glob(directorio_entrada+"*.RData")) ): 
+for k in range(1,len(glob.glob(directorio_entrada+"*.RData"))+1 ): 
     archivo="tld"+str(k)+".RData"
     enlace=directorio_entrada + archivo
-    original = pyreadr.read_r(enlace)
+    #Pongo try except.. porque existen archivos que dan error al momento de leerlos
+    try:
+       original = pyreadr.read_r(enlace)
+    except:
+        continue
+        
     if('tabla_variables' not in globals() ):
          tabla_variables=pd.DataFrame()
          tabla_variables = pd.DataFrame( columns=['dateTimeLine','latitude','longitude','accuracy','altitude','verticalAccuracy','velocity','heading','dateActivity','activity1','confidence1','activity2','confidence2','file'])
     for i in range(0,len(original['dataSource']['dateTimeLine'])): 
-                tabla_variables = tabla_variables.append({'dateTimeLine':original['dataSource']['dateTimeLine'][i],'latitude':original['dataSource']['latitude'][i],'longitude':original['dataSource']['longitude'][i],'accuracy':original['dataSource']['accuracy'][i],'verticalAccuracy':original['dataSource']['verticalAccuracy'][i],'altitude':original['dataSource']['altitude'][i],'velocity':original['dataSource']['velocity'][i],'heading':original['dataSource']['heading'][i],'dateActivity':original['dataSource']['dateActivity'][i],'activity1':original['dataSource']['activity1'][i],'confidence1':original['dataSource']['confidence1'][i],'activity2':original['dataSource']['activity2'][i],'confidence2':original['dataSource']['confidence2'][i],'file':archivo}, ignore_index=True)          
+                    #existen archivos que solo tienen 3 parametros, que son, dateTimeLine,Latitude y Longitude
+                    #se puede anexar estos datos de estos archivos o solo ignorarlos
+                    if((original['dataSource'].shape[1])<4):
+                     #tabla_variables = tabla_variables.append({'dateTimeLine':original['dataSource']['dateTimeLine'][i],'latitude':original['dataSource']['latitude'][i],'longitude':original['dataSource']['longitude'][i],'accuracy':None,'verticalAccuracy':None,'altitude':None,'velocity':None,'heading':None,'dateActivity':None,'activity1':None,'confidence1':None,'activity2':None,'confidence2':None,'file':archivo}, ignore_index=True)          
+                     continue
+                    else:                    
+                     tabla_variables = tabla_variables.append({'dateTimeLine':original['dataSource']['dateTimeLine'][i],'latitude':original['dataSource']['latitude'][i],'longitude':original['dataSource']['longitude'][i],'accuracy':original['dataSource']['accuracy'][i],'verticalAccuracy':original['dataSource']['verticalAccuracy'][i],'altitude':original['dataSource']['altitude'][i],'velocity':original['dataSource']['velocity'][i],'heading':original['dataSource']['heading'][i],'dateActivity':original['dataSource']['dateActivity'][i],'activity1':original['dataSource']['activity1'][i],'confidence1':original['dataSource']['confidence1'][i],'activity2':original['dataSource']['activity2'][i],'confidence2':original['dataSource']['confidence2'][i],'file':archivo}, ignore_index=True)          
      
