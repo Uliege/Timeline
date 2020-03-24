f9bf22ca6a3f5f2af95b8b7593f15803acc91f4a
malcivar <44213991+malcivar@users.noreply.github.com>
2020-03-23 21:22:45 -0500

    lectura de archivos optima
    lectura de archivos optima

--- DataFrame2.py
+++ DataFrame2.py
@@ -22,32 +22,25 @@ for k in range(0,len(glob.glob(directorio_entrada+"*.RData")) ):
     #Pongo try except.. xq existen archivos que dan error al momento de leerlos
     try:
        original = pyreadr.read_r(enlace)
-       copia = pyreadr.read_r(enlace)
     except:
         print("archivo dañado---> " +str(archivo) )
         continue
+    print("Tamaño del archivo: "+str(len(original['dataSource'])))
     
     #Inicializa dataframe y comprueba si ya existe
     if('tabla_final_uce' not in globals() and 'tabla_final_todo' not in globals() ):
          tabla_final_uce=pd.DataFrame()
          tabla_final_uce=pd.DataFrame(columns=[])
-         tabla_final_todo=pd.DataFrame()
-         tabla_final_todo=pd.DataFrame(columns=[])
     #Comprueba numero de columnas del dataframe original y lo concatena a un nuevo dataframe    
     if((original['dataSource'].shape[1])<13):    
            continue
     else:
        original['dataSource']['file']=archivo
-       copia['dataSource']['file']=archivo
-    #Delimitar dataframe al area de la Universidad
-       for p in range(0, len(original['dataSource'])):
-          if( ((-0.19425 >= original['dataSource']['latitude'][p] >= -0.20341)and (-78.49805 >= original['dataSource']['longitude'][p] >= -78.51377))):    
-            continue
-          else:
-             original['dataSource'].drop(index=[p], inplace=True)
+       original['dataSource']= original['dataSource'].drop( original['dataSource'][((original['dataSource']['latitude']>=-0.19425) | (original['dataSource']['latitude'] <= -0.20341))|(( original['dataSource']['longitude']>=-78.49805 )|(original['dataSource']['longitude'] <= -78.51377))].index)
+
             
     tabla_final_uce=  tabla_final_uce.append([ original['dataSource']], ignore_index=True)
-    tabla_final_todo=  tabla_final_todo.append([ copia['dataSource']], ignore_index=True)
+  
     
 #Guardar dataframe en un archivo dat
 tabla_final_uce.to_csv(directorio_salida+archivo_salida)  
